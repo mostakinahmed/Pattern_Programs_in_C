814d04b7ae99f13148413f3602062421016f4530
Mostakin Ahmed <105800326+mostakinahmed@users.noreply.github.com>
2025-04-30 22:33:43 +0600

    Update 9. Inverted half pyramid of star.c

--- 9. Inverted half pyramid of star.c
+++ 9. Inverted half pyramid of star.c
@@ -3,6 +3,7 @@ int main() {
    int i, j, rows;
    printf("Enter the number of rows: ");
    scanf("%d", &rows);
+   //row and col
    for (i = rows; i >= 1; --i) {
       for (j = 1; j <= i; ++j) {
          printf("* ");

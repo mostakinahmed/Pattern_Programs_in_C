2f7753c7412334840924ecb4f06d4f7ce0ac384d
Mostakin Ahmed <105800326+mostakinahmed@users.noreply.github.com>
2025-04-30 22:34:07 +0600

    Update 6. Half Pyramid of star.c

--- 6. Half Pyramid of star.c
+++ 6. Half Pyramid of star.c
@@ -3,6 +3,7 @@ int main() {
    int i, j, rows;
    printf("Enter the number of rows: ");
    scanf("%d", &rows);
+   //row and col
    for (i = 1; i <= rows; ++i) {
       for (j = 1; j <= i; ++j) {
          printf("* ");
